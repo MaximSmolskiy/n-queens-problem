1438f77dc1f0bf5b9fc82f7b2ac4901c5cc8bf6f
MaximSmolskiy <mithridatus@mail.ru>
2025-07-14 16:43:56 +0300

    Refactor unit test

--- test/unit/test_solve.py
+++ test/unit/test_solve.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from os import path
 import sys
@@ -6,24 +7,24 @@ sys.path.append(path.abspath(path.join('..', '..', 'src')))
 from solve import solve
 
 
-def test_solve():
-    for n in range(1, 11):
-        board = solve(n)
+@pytest.mark.parametrize('n', range(1, 11))
+def test_solve(n):
+    board = solve(n)
 
-        if n in [2, 3]:
-            assert np.sum(board) == n - 1
-        else:
-            assert np.sum(board) == n
+    if n in [2, 3]:
+        assert np.sum(board) == n - 1
+    else:
+        assert np.sum(board) == n
 
-        for row in range(n):
-            assert np.sum(board[row, :]) <= 1
+    for row in range(n):
+        assert np.sum(board[row, :]) <= 1
 
-        for col in range(n):
-            assert np.sum(board[:, col]) <= 1
+    for col in range(n):
+        assert np.sum(board[:, col]) <= 1
 
-        for diag in range(n):
-            assert np.sum(board.diagonal(diag)) <= 1
-            assert np.sum(board.diagonal(-diag)) <= 1
+    for diag in range(n):
+        assert np.sum(board.diagonal(diag)) <= 1
+        assert np.sum(board.diagonal(-diag)) <= 1
 
-            assert np.sum(np.fliplr(board).diagonal(diag)) <= 1
-            assert np.sum(np.fliplr(board).diagonal(-diag)) <= 1
+        assert np.sum(np.fliplr(board).diagonal(diag)) <= 1
+        assert np.sum(np.fliplr(board).diagonal(-diag)) <= 1
