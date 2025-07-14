f3a94a52875b7cfcb4b6353fc51ad15cd17175a9
MaximSmolskiy <mithridatus@mail.ru>
2025-07-14 18:40:09 +0300

    Add solution

---
+++ src/solve.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy.optimize as opt
+
+
+def solve(n):
+    c = -np.ones(n * n)
+    integrality = np.ones(n * n)
+    bounds = opt.Bounds(lb=0, ub=1)
+
+    A = []
+    for row in range(n):
+        board = np.zeros((n, n))
+        board[row, :] = 1
+        A.append(board.ravel())
+
+    for col in range(n):
+        board = np.zeros((n, n))
+        board[:, col] = 1
+        A.append(board.ravel())
+
+    ones = np.ones((n, n))
+    for diag in range(n):
+        A.append(np.diag(ones.diagonal(diag), diag).ravel())
+        A.append(np.diag(ones.diagonal(-diag), -diag).ravel())
+
+        A.append(np.fliplr(np.diag(ones.diagonal(diag), diag)).ravel())
+        A.append(np.fliplr(np.diag(ones.diagonal(-diag), -diag)).ravel())
+
+    constraints = opt.LinearConstraint(A=A, ub=1)
+
+    res = opt.milp(c=c, integrality=integrality, bounds=bounds, constraints=constraints)
+    return res.x.reshape((n, n)) >= 1e-6
